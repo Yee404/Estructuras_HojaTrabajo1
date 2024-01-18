0a4f6e09e55fe99ac5b762a506ab15547345433c
Yee404 <yee231193@uvg.edu.gt>
2024-01-18 11:34:03 -0600

    commit1
    Prueba de estructura

---
+++ AM.java
@@ -0,0 +1,5 @@
+public class AM {
+    // condicion múltiplos de 10 entre 530 y 1610 para dial
+    // dividio 10 == 0, sí es, else ignorar.
+
+}
---
+++ RADIO.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class RADIO {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean go = true;
+        String opcion = "";
+
+        while (go) {
+            printMenu();
+
+            System.out.println("Ingrese la opción seleccionada: ");
+            opcion = scan.nextLine();
+            System.out.println("");
+        }
+    }
+
+    // Menú de la radio (para la terminal, no interfaz gráfica)
+    public static void printMenu() {
+        System.out.println("");
+
+    }
+}
