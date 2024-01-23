4f5b2d315c138c7a1a2ba7a72a0826535131b4f8
Yee404 <yee231193@uvg.edu.gt>
2024-01-23 17:55:31 -0600

    commit
    Se agregó los métodos setFavFrequency y getFavFrecuency, se cambiaron las variables de doble a float para que coincida con la interfaz

--- RadioApp.java
+++ RadioApp.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class RadioApp {
-
     public static void main(String[] args) {
         Radio radio = new Radio();
         Scanner scanner = new Scanner(System.in);
@@ -14,6 +13,8 @@ public class RadioApp {
             System.out.println("3. Avanzar a la siguiente estación");
             System.out.println("4. Retroceder a la estación anterior");
             System.out.println("5. Ver estado actual");
+            System.out.println("6. Guardar estación favorita");
+            System.out.println("7. Obtener estación favorita");
             System.out.println("0. Salir");
             System.out.print("Ingrese la opción: \n");
 
@@ -40,6 +41,12 @@ public class RadioApp {
                 case 5:
                     radio.showCurrentState();
                     break;
+                case 6:
+                    radio.setFavFrequency();
+                    break;
+                case 7:
+                    radio.getFavFrequency();
+                    break;
                 case 0:
                     System.out.println("Saliendo...");
                     break;
@@ -55,15 +62,19 @@ public class RadioApp {
 class Radio {
     private boolean encendido;
     private boolean amMode;
-    private double frecuenciaActual;
-    private double lastAMStation = 530;
-    private double lastFMStation = 87.9;
+    private float frecuenciaActual;
+    private float lastAMStation = 530f;
+    private float lastFMStation = (float) 87.9f;
+    private float[] Favoritas = new float[12];
+    private Scanner scanner = new Scanner(System.in);
+    private int button;
 
     public Radio() {
         encendido = false;
         amMode = true;
-        frecuenciaActual = 530;
+        frecuenciaActual = 530f;
     }
+
 
     public boolean getState() {
         return encendido;
@@ -86,9 +97,11 @@ class Radio {
                 }
             } else {
                 if (frecuenciaActual < 87.9 || frecuenciaActual > 107.9) {
-                    frecuenciaActual = 87.9;
+                    frecuenciaActual = (float) 87.9;
                 }
             }
+        } else {
+            System.out.println("\u001B[31m" + "El radio se encuentra apagado." + "\u001B[37m");
         }
     }
 
@@ -105,9 +118,12 @@ class Radio {
                 frecuenciaActual = Math.min(frecuenciaActual + 10, 1610);
                 lastAMStation = frecuenciaActual;
             } else {
-                frecuenciaActual = Math.min(frecuenciaActual + 0.2, 107.9);
+                frecuenciaActual = (float) Math.min(frecuenciaActual + 0.2, 107.9);
                 lastFMStation = frecuenciaActual;
             }
+        }
+        else {
+            System.out.println("\u001B[31m" + "El radio se encuentra apagado." + "\u001B[37m");
         }
     }
 
@@ -117,9 +133,57 @@ class Radio {
                 frecuenciaActual = Math.max(frecuenciaActual - 10, 530);
                 lastAMStation = frecuenciaActual;
             } else {
-                frecuenciaActual = Math.max(frecuenciaActual - 0.2, 87.9);
+                frecuenciaActual = (float) Math.max(frecuenciaActual - 0.2, 87.9);
                 lastFMStation = frecuenciaActual;
             }
         }
+        else {
+            System.out.println("\u001B[31m" + "El radio se encuentra apagado." + "\u001B[37m");
+        }
+
+    }
+
+    public void setFavFrequency() {
+
+        if (encendido) {
+            System.out.println("Ingrese el botón en donde desea guardarlo: ");
+            button = scanner.nextInt();
+
+            if (button >= 1 && button <= 12) {
+                // se coloca -1 para que el botón 1 coincida con el índice 0 y así sucesivamente
+                Favoritas[button - 1] = frecuenciaActual;
+                System.out.println("Se ha guardado su estación favorita con éxito");
+            } else {
+                System.out.println(
+                        "Solamente existen 12 botones para guardar estaciones. \nIngrese el botón en donde desea guardarlo: ");
+            }
+        } else {
+            System.out.println("\u001B[31m" + "El radio se encuentra apagado." + "\u001B[37m");
+        }
+    }
+
+    public float getFavFrequency() {
+        if (encendido) {
+            System.out.println("Ingrese el botón de la estación que desea: ");
+            button = scanner.nextInt();
+
+            if (button >= 1 && button <= 12) {
+                if (Favoritas[button - 1] == 0.0) {
+                    System.out.println("No se ha guardado ninguna estación en este botón");
+                } else {
+                    frecuenciaActual = Favoritas[button - 1];
+                    System.out.println(frecuenciaActual);
+                }
+
+            } else {
+                System.out.println(
+                        "Solamente existen 12 botones para guardar estaciones. \nIngrese el botón en donde desea guardarlo: ");
+                return -1f;
+            }
+        } else {
+            System.out.println("\u001B[31m" + "El radio se encuentra apagado." + "\u001B[37m");
+        }
+        return -1f;
+
     }
 }
